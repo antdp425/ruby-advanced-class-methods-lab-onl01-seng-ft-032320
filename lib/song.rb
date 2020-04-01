0d523739847ea6a0af082980c19953984bb91a1a
Anthony DiPietrantonio <antdp425@aol.com>
2020-04-01 18:08:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
   end
 
   def self.find_or_create_by_name(song_name)
-    if self.find_By_name(song_name)
+    if self.find_by_name(song_name)
       self.find_by_name
     else
       self.create_by_name(song_name)
